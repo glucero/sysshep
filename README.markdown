dc59cb5836dc6706998b88d48a6641f82cfa77bd
Gino Lucero <glucero@gmail.com>
2014-06-13 22:52:10 -0700

    test datapoints, xml and a basic readme

---
+++ README.markdown
@@ -0,0 +1,7 @@
+SysShep
+======
+
+requirements
+--------
+Ruby 2.1.2/JRuby 1.7.12 (with Java 7)
+
---
+++ spec/datapoints.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo -e "App|Test|Metric|A\t1.23\t0"
+echo -e "App|Test|Metric|B\t4.56\t1\tWarning message"
+echo -e "App|Test|Metric|C\t7.89\t2\tCritical message"
