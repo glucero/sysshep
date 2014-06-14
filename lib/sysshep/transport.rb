66802139a45782319602bbeaa88c50b5bb58f347
Gino Lucero <glucero@gmail.com>
2014-06-14 14:16:29 -0700

    transport module for errors, mime, port and scheme information

---
+++ lib/sysshep/transport.rb
@@ -0,0 +1,23 @@
+module SysShep::Transport
+
+  SCHEME = 'https'.freeze
+  PORT   = 443
+
+  NotAuthorized = Class.new(StandardError)
+  RequestFailed = Class.new(StandardError)
+
+  MIME = Hash.new(  'application/json'.freeze ).merge(
+    kernel:         'application/vnd.absperf.sskj1+json'.freeze,
+    measurements:   'application/vnd.absperf.ssmj1+json'.freeze,
+    escalations:    'application/vnd.absperf.ssaj1+json'.freeze,
+    configurations: 'application/vnd.absperf.sscj1+json'.freeze
+  )
+
+  ServiceMain = -> fqdn { "https://core.#{fqdn}/service_descriptors" }
+
+  extend Forwardable
+
+  def_delegators :@credentials, :username, :password, :portal, :agent
+
+end
+
